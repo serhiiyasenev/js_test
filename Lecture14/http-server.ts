f93edf9e23450781d5f8d5fe97781afd92094e0b
serhii.yasenev@ihsmarkit.com <serhii.yasenev@ihsmarkit.com>
2022-12-07 23:30:19 +0200

    update

--- Lecture14/http-server.ts
+++ Lecture14/http-server.ts
@@ -2,7 +2,7 @@ const http = require('http');
 
 const requestListener = function (req, res) {
     res.writeHead(200);
-    res.end('response');
+    res.end(`relative url is: '${req.url.split('/')[1]}'`);
 }
 
 const server = http.createServer(requestListener);
